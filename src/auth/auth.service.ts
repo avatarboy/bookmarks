9e39b333668d46a9bbc856127dff9387c920c23a
avatarboy <herydevrk@gmail.com>
2022-10-15 02:53:55 +0200

    get started with NestJS

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService {
+
+    signup() {
+        return { message: 'I have signed up' };
+    }
+
+    signin() {
+        return { message: 'I have sign in' };
+    }
+
+}
