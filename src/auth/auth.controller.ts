6b6336bde42b9bb90428911098d33b9315fe0c2a
avatarboy <herydevrk@gmail.com>
2022-10-16 00:19:08 +0200

    Nest global pipe ft:hashing password with argon

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
-import { Controller, Post, Req } from "@nestjs/common";
-import { Request } from "express";
+import { Controller, Post, Body } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController {
@@ -8,8 +8,7 @@ export class AuthController {
     constructor(private authService: AuthService){}
 
     @Post('signup')
-    signup(@Req() req: Request) {
-        console.log(req);
+    signup(@Body() dto: AuthDto ) {
         
         return this.authService.signup();
     }
